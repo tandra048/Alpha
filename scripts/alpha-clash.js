ab08988ce388a0356144c7b17c5297318a53c992
tandra048 <2048tandraju@gmail.com>
2025-05-23 23:06:56 +0600

    done play ground part

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,1 +1,50 @@
-console.log('hi');
+// function play(){
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+//    //console.log('hi'); 
+
+//    const playGround = document.getElementById('play-ground');
+//    playGround.classList.remove('hidden');
+// }
+function continueGame(){
+const alphabet = getARandomAlphabet();
+console.log(alphabet);
+const currentAlphabetElement = document.getElementById('current-alphabet');
+currentAlphabetElement.innerText = alphabet;
+
+//set background color
+setBackgroundColorById(alphabet);
+}
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
+
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+   element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    //console.log(alphabets);
+
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    
+    const alphabet = alphabets[index];
+    //console.log(index, alphabet);
+    return alphabet;
+}
